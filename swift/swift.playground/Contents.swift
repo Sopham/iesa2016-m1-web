cedfba181ee99c99b8e371dae9864b96b2860ace
Mastere 1 IT <mastere1it@iMacdeAutres50.sto-002.saint-augustin.in.iesanetwork.com>
2016-06-20 13:06:12 +0200

    creation dossier oop

--- swift/swift.playground/Contents.swift
+++ swift/swift.playground/Contents.swift
@@ -28,3 +28,65 @@ class NamedShape {
 
 var carre = NamedShape(name: "drapeau")
 
+
+//26
+
+class Square : NamedShape{
+
+    var sideLength : Double
+    init (length :  Double, name: String) {
+        self.sideLength = length
+        super.init(name: name)
+        sidesCount = 4
+}
+
+    func area() -> Double{
+            return sideLength * sideLength
+        
+}
+
+    override func getInfo() -> String {
+        return "Square : L=\(sideLength)."
+    
+    }
+
+}
+
+let s = Square(length: 5.2, name: "myS")
+s.area()
+
+s.getInfo()
+
+
+
+
+class Instrument {
+    
+    var type  : String
+    var poids : Int = 0
+    
+    init(name: String) { self.type = name}
+    
+    func getType() -> String {
+        return "test"
+    }
+    
+}
+
+
+class Guitare : Instrument{
+    
+    var nbCordes : Int = 0
+    
+    // override init(name: String) { super.init(name: name)}
+    
+    
+    func getCords() -> Int {
+        return self.getCords()
+    }
+
+    
+}
+
+var guitare = Guitare (name: "electrik")
+
